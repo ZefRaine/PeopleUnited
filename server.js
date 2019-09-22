a473726863c11a2794e204f657a2431c1f59c068
zeftambornino@gmail.com <zeftambornino@gmail.com>
2019-09-22 18:47:49 -0500

    created htmlRoutes and html pages

--- server.js
+++ server.js
@@ -2,5 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
+require("./routing/htmlRoutes.js")(
+    app
+)
 const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+})
